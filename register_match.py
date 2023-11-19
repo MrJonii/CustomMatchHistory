01b5b0164fab1e35830c1b1d680f292e110c9b4b
Maciej Barwik <barwik1999@gmail.com>
2023-11-19 01:01:50 +0000

    security

--- register_match.py
+++ register_match.py
@@ -10,6 +10,20 @@ with open(LOCKFILE) as file:
     port = content.split(':')[2]
     password = content.split(':')[3]
 
+with open('backend/.env') as file:
+    env = file.readlines()
+    backend_username = env.split('\n')
+
+    for line in env.split('\n'):
+        split = line.split('=')
+
+        if split[0] == 'USER':
+            backend_username = split[1]
+
+        if split[0] == 'PASSWORD':
+            backend_password = split[1]
+
+
 url = f'https://127.0.0.1:{port}/lol-match-history/v1/games/{sys.argv[1]}'
 auth = f"Basic {base64.b64encode(f'riot:{password}'.encode('ascii')).decode('ascii')}"
 
@@ -18,6 +32,6 @@ print(response.status_code)
 
 url = f'http://127.0.0.1:42069/api/matches'
 
-response = requests.post(url, headers = {'Authorization': 'admin:auD79ERBjYLLikjeNvAL'}, json = response.json())
+response = requests.post(url, headers = {'Authorization': f'{backend_username}:{backend_password}'}, json = response.json())
 print(response.status_code)
 print(response.json())
